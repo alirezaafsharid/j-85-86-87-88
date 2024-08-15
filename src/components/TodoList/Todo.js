9e0acb92a994ff6b1fe4d9382d19ae87d3d64f7f
alireza <afshar2451382@gmail.com>
2024-08-15 16:31:41 +0330

    اتمام پروژه

--- src/components/TodoList/Todo.js
+++ src/components/TodoList/Todo.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Todo extends Component {
+  removeClickHandler(id) {
+    this.props.onRemove(id);
+  }
 
-    render() {
-        return (
-            // 'completed' class for completed todos
-            <div className='todo' style={{ display: 'flex' }}>
-                <li className="todo-item">{this.props.title}</li>
+  editClickHandler(id) {
+    this.props.onEdit(id);
+  }
 
-                <button className="check-btn">
-                    <i className="fas fa-check" aria-hidden="true"></i>
-                </button>
+  render() {
+    return (
+      // 'completed' class for completed todos
+      <div
+        className={`todo ${this.props.completed ? "completed" : ""}`}
+        style={{ display: "flex" }}
+      >
+        <li className="todo-item">{this.props.title}</li>
 
-                <button className="trash-btn">
-                    <i className="fas fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
-        )
-    }
+        <button
+          className="check-btn"
+          onClick={this.editClickHandler.bind(this, this.props.id)}
+        >
+          <i className="fas fa-check" aria-hidden="true"></i>
+        </button>
+
+        <button
+          className="trash-btn"
+          onClick={this.removeClickHandler.bind(this, this.props.id)}
+        >
+          <i className="fas fa-trash" aria-hidden="true"></i>
+        </button>
+      </div>
+    );
+  }
 }
